7da5bd12486980cf3f86b790d0b7ee8f18756521
harshit <harshitagarwalsmt@gmail.com>
2023-09-12 01:36:58 +0530

    5/53

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,30 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :trackable 
+         :recoverable, :rememberable, :validatable, :trackable
+  PROFILE_TITLE = [
+    'Senior Ruby On Rails Developer',
+    'Full Stack Ruby On Rails Developer',
+    'Junior Full Stack Ruby On Rails Developer',
+    'Senior Full Stack Ruby On Rails Developer',
+    'Senior Java Developer',
+    'Senior Frontend Developer',
+    'Frontend Developer',
+    'Applied Scientist I',
+    'Devops Engineer',
+    'Cloud Engineer',
+    'Android Engineer',
+    'QA Engineer',
+    'Software Development Engineer',
+    'Software Development Engineer II',
+    'Software Development Engineer in Test',
+    'Technical Architect',
+    'Tech Lead',
+    'Staff Software Engineer',
+    'Associate Software Engineer',
+    'Data Scientist',
+    'Big Data Engineer',
+    'Technical Support Engineer',
+    'UI Engineer'
+  ].freeze
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+ActiveRecord::Base.transaction do
+	500.times do |i|
+		user = User.create(
+			first_name: Faker::Name.first_name,
+			last_name: Faker::Name.last_name,
+			username: "#{Faker::Name.first_name.downcase}_#{i+1}",
+			email: Faker::Internet.email,
+			contact_number: Faker::PhoneNumber.phone_number_with_country_code,
+			street_address: Faker::Address.street_address,
+			city: Faker::Address.city,
+			state: Faker::Address.state,
+			country: Faker::Address.country,
+			pincode: Faker::Address.postcode,
+			date_of_birth: (Date.today + rand(1..30).days) - rand(24..36).years,
+			profile_title: User::PROFILE_TITLE.sample,
+			password: 'password',
+			about: Faker::Lorem.paragraphs
+		)
+		puts "User #{i+1} created succesfully"
+	end
+end
