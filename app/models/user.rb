cb5ba24a9bdf660d498fc2e947ee076f2ccfc136
harshit <harshitagarwalsmt@gmail.com>
2023-09-12 03:16:29 +0530

    8/53

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,4 +31,10 @@ class User < ApplicationRecord
   def name
     "#{first_name} #{last_name}".strip
   end
+  def self.ransackable_attributes(auth_object = nil)
+    ['city', 'country']
+  end
+  def self.ransackable_associations(auth_object = nil)
+    []
+  end
 end
