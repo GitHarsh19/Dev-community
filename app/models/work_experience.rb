ea4c086817ed2e2de4b0fe58302441a4a8e8c9bb
harshit <harshitagarwalsmt@gmail.com>
2023-09-14 17:54:40 +0530

    25/54

--- app/models/work_experience.rb
+++ app/models/work_experience.rb
@@ -1,7 +1,16 @@
 class WorkExperience < ApplicationRecord
-  belongs_to :user
-  validates :company, :start_date, :employment_type, :job_title, :location, :location_type, presence: true
+  
+  EMPLOYMENT_TYPE = ['Full-time', 'Part-time', 'Self-employeed', 'Freelance', 'Trainee', 'Internship']
+  LOCATION_TYPE = ['On-site', 'Hybrid', 'Remote']
 
+
+  belongs_to :user
+  validates :company, :start_date, :job_title, :location, presence: true
+
+  validates :employment_type, presence: true, inclusion: { in: EMPLOYMENT_TYPE, message: 'Not A Valid Employment Type'}
+  
+  validates :location_type, presence: true, inclusion: { in: LOCATION_TYPE, message: 'Not A Valid Location Type' }
+  
   validate :work_experience_last_date
   validate :presence_of_end_date
   validate :end_date_greater_than_start_date, if: :currently_not_working_here?
